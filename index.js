08969e0bbc89cbb536e9e78d11e9e05e395701f7
Wee Kian <weekian.yeam@gmail.com>
2019-02-07 18:55:26 +0800

    Setup of Docker
    - Moved heroku dockerfile to separate dir
    - Updated heroku.yml to point to non-root dockerfile
    - Modified root dockerfile for local development

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const Telegraf = require('telegraf');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("App listening to " + process.env.PORT);
+})
 
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
 
