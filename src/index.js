248627de004a7dbaff4d268f6d0dd7b2681bb90d
Wee Kian <weekian.yeam@gmail.com>
2019-02-08 03:22:06 +0800

    Setup Flow and Babel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-const x = 45;
+// @flow
+import Telegraf from 'telegraf';
+import express from 'express';
+import * as utils from './helper/index.js'
+const app = express();
 
-console.log(x);
+app.get('/', (req, res) => {
+    res.send(utils.generateResponse());
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening to ${process.env.PORT || 8080}`);
+})
+
+const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
+
+bot.start((ctx) => ctx.reply(`[${process.env.NODE_ENV || 'development'}] Hello World @ ${(new Date()).toString()}`));
+bot.launch();
