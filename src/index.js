4858b5bb8a03a38079073181baa65044bc827a63
Wee Kian <weekian.yeam@gmail.com>
2019-02-24 18:52:12 +0800

    Refactoring of index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+// Entrypoint to starting bot application
 import Server from "./server";
 import Bot from "./bot";
+import Database from "./db/db";
 
-const { PORT, NODE_ENV, TELEGRAM_TOKEN } = process.env;
+const { PORT, NODE_ENV, TELEGRAM_TOKEN, DATABASE_URL } = process.env;
 
-const server = new Server(PORT || "8080", NODE_ENV || "development");
-const bot = new Bot(TELEGRAM_TOKEN, NODE_ENV || "development");
+const server = new Server(PORT, NODE_ENV);
+const bot = new Bot(TELEGRAM_TOKEN, NODE_ENV, Database(DATABASE_URL));
 
 server.start();
 bot.start();
