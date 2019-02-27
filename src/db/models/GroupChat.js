ba347bcd5129fcfb2bd36a2ca9bd28b2a7769918
Wee Kian <weekian.yeam@gmail.com>
2019-02-28 02:15:54 +0800

    Removed unnecessary type attribute

--- src/db/models/GroupChat.js
+++ src/db/models/GroupChat.js
@@ -4,9 +4,5 @@ export default (sequelize, DataTypes) => {
             type: DataTypes.INTEGER,
             primaryKey: true,
         },
-        kind: {
-            type: DataTypes.ENUM("group"),
-            allowNull: false,
-        },
     });
 };
