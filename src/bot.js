4f3a6f49e35ba67e62358ab48d43f2987a0b7062
Wee Kian <weekian.yeam@gmail.com>
2019-02-28 02:16:20 +0800

    Implemented bot logic

--- src/bot.js
+++ src/bot.js
@@ -1,36 +1,102 @@
-import Telegraf from "telegraf";
+/*
+Note
+If ctx.message.group_chat_created is true means create with group chat
+If ctx.message.new_chat_members contains bot then is added to group chat
+If ctx.message.left_chat_member is bot's id then bot is deleted
+ */
 
 export default class Bot {
-    constructor(token, cache, db) {
-        // Register token
-        this.bot = new Telegraf(token);
-        this.db = db;
+    constructor(
+        Telegram,
+        token,
+        env,
+        database,
+        addedToGroupHandler,
+        privateCommands = [],
+        groupCommands = []
+    ) {
+        // Initialize Telegram Bot
+        this.bot = new Telegram(token);
 
-        // Add cache object to middleware
-        this.bot.use(cache.middleware);
+        // Store database object and related Models
+        this.database = database.db;
+        this.Person = database.Person;
+        this.GroupChat = database.GroupChat;
+        this.Session = database.Session;
 
-        // Start handlers for handling adding into chat and leaving chat
-        this.registerChatAddHandler();
-        this.registerGroupChatLeaveHandler();
-    }
+        // Registering of telegram commands
+        this.registerPrivateCommands(privateCommands);
+        this.registerGroupCommands(groupCommands);
+        this.handleAddedToGroup(addedToGroupHandler);
 
-    start() {
-        this.bot.launch();
-    }
-
-    registerChatAddHandler() {
-        // Saves to redis and db
-        // distinguishes between private and non-private chats
-    }
-
-    registerChatLeaveHandler() {
-        // Applies to non-private chat, deletes group-related information stored in db
-    }
-
-    registerPrivateChatCommandHandler(command, handler) {
-        this.bot.command(command, (ctx) => {
-            // check against cache if chat is private. If not private, reply error message
-            ctx.reply(handler.process());
+        this.bot.on("message", (ctx) => {
+            console.log("Unknown command received");
+            console.log(JSON.stringify(ctx.message, null, 4));
+            console.log(JSON.stringify(ctx.chat, null, 4));
         });
+    }
+
+    // Command handlers for private chats
+    registerPrivateCommands(privateCommands) {
+        privateCommands.forEach((command) => {
+            this.bot.command(command.name, (ctx) => {
+                if (this.isPrivateChat(ctx.chat)) {
+                    ctx.reply(
+                        command.process({
+                            message: ctx.message,
+                            from: ctx.from,
+                            database: this.database,
+                            Person: this.Person,
+                            GroupChat: this.GroupChat,
+                            Session: this.Session,
+                        })
+                    );
+                } else {
+                    ctx.reply(
+                        "Oops, this command is reserved for private chats only. Send /help unsure"
+                    );
+                }
+            });
+        }, this);
+    }
+
+    // Command handlers for group chats
+    registerGroupCommands(groupCommands) {
+        groupCommands.forEach((command) => {
+            this.bot.command(command.name, (ctx) => {
+                if (!this.isPrivateChat(ctx.chat)) {
+                    ctx.reply(
+                        command.process({
+                            database: this.database,
+                            Person: this.Person,
+                            GroupChat: this.GroupChat,
+                            Session: this.Session,
+                        })
+                    );
+                } else {
+                    ctx.reply(
+                        "Oops, this command is reserved for group chats only. Send /help if unsure"
+                    );
+                }
+            });
+        }, this);
+    }
+
+    // Command handlers for add to group
+    handleAddedToGroup(handler) {
+        // logic here
+    }
+
+    isPrivateChat(chat) {
+        return chat && chat.type && chat.type === "private";
+    }
+
+    async start() {
+        // Configure to group chat commands that contain bot username
+        const botInfo = await this.bot.telegram.getMe();
+        this.bot.options.username = botInfo.username;
+
+        // Starts listening for messages
+        this.bot.startPolling();
     }
 }
