aea4f7bf03733c4c6df930752d29e78d378a1b32
Wee Kian <weekian.yeam@gmail.com>
2019-03-04 01:54:14 +0800

    Implemented /checkin command
    - Added unit tests

--- src/commands/checkin.js
+++ src/commands/checkin.js
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 export default {
     name: "checkin",
     process: async ({
@@ -8,6 +10,45 @@ export default {
         GroupChat,
         Session,
     }) => {
-        return "checkin";
+        // finds person or creates new if doesn't exist
+        const [person] = await Person.findOrCreate({
+            where: {
+                id: from.id,
+            },
+            defaults: {
+                name: from.first_name,
+            },
+        });
+
+        const hasPendingCheckouts =
+            (await Session.count({
+                where: {
+                    checkoutTimestamp: {
+                        $eq: null,
+                    },
+                },
+            })) !== 0;
+
+        if (hasPendingCheckouts) {
+            return `Hi ${
+                person.name
+            }, you have an ongoing session. Do you mean /checkout?`;
+        }
+        // Converts Unix (utc) timestamp to SG time
+        const localTime = moment
+            .unix(message.date)
+            .utc()
+            .utcOffset("+08:00");
+
+        // Creates Session associated to person
+        const session = await person.createSession({
+            checkinTimestamp: localTime.toDate(),
+        });
+
+        const displayLocalTime = moment(session.checkinTimestamp).format(
+            "h:mA [on] dddd, Do MMMM YYYY"
+        );
+
+        return `Hi ${person.name}, you have checked in at ${displayLocalTime}`;
     },
 };
